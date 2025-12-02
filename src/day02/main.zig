0ba6cf4a6cc4b1ab99c155f2464f3c0c4df6bad9
LazyTurtle33 <jro0309@gmail.com>
2025-12-02 13:56:48 +0200

    day two setup

---
+++ src/day02/main.zig
@@ -0,0 +1,8 @@
+﻿const std = @import("std");
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Advent of Code - Day 2\n", .{});
+
+    // TODO: implement solution for day 2
+}
